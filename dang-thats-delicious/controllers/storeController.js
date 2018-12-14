1f3013749367d437614ce217e04c7a9da9f3e84c
Bobby Erickson <ericksonr07@gmail.com>
2018-12-14 00:38:43 -0500

    Post to MongoDB
    - Created our Add Store page along with the /add route.
    - Utilized a mixin as our form for the Add Store page.
    - Added the catchErrors function from errorHandlers to routes/index.js
    - POST method added to index.js, which is calling our storeController.
    - createStore function added to our controller.  This allows us to save
    to our DB.  It is utilizing async await.

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,16 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store'); //this is grabbing our Store model from start.js
+
 exports.homePage = (req, res) => {
     res.render('index');
+};
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {title: 'Add Store'});
+};
+
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    res.redirect('/');
 };
